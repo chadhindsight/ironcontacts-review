4cc21736dcede8529859c986e22af24359c13aa5
Chad Hinds <chadrickj8@gmail.com>
2020-09-23 10:12:50 -0400

    New bug introduced

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Header from './components/Header'
 function App() {
   // Use hooks for initial state
   const [actors, setActors] = useState([])
-  const [remainingContacts] = useState(contacts)
+  // Need a way to update remaining contacts with second arg
+  const [remainingContacts, setRemainingContacts] = useState(contacts)
   console.log(typeof actors, actors)
 
-
+  // Similar to ComponentDidMount and ComponentDidUpdate
+  // Empty arr tells React that your effect doesn't depend on any values from props or state, so it never needs to re-run
   useEffect(() => {
     setActors(contacts.slice(0, 5))
+    setRemainingContacts(contacts)
   }, []);
 
   function displayActors(data) {
@@ -26,16 +29,20 @@ function App() {
     })
   }
 
-  //Logic that shows a random contact
+  //Logic that adds a random contact
   function randomizeContact(list) {
     // Select random contact and put it to the start of contact list
-    let randomContact = remainingContacts[Math.floor(Math.random() * remainingContacts.length)]
+    let randoNum = Math.floor(Math.random() * remainingContacts.length)
+    let randomContact = remainingContacts[randoNum]
 
     // Pass new array to setActors
     list.unshift(randomContact)
     setActors([...list])
-    // Update list of remaining contacts
 
+    // Update list of remaining contacts
+    let remainingContactsCopy = [...remainingContacts].splice(randoNum, randoNum + 1)
+    setRemainingContacts(remainingContactsCopy)
+    console.log(randoNum)
   }
 
   return (
